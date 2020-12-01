07022ed4bf4ef54ab2b48f51b86d002f530f97e7
Anthony Lee <42955348+anthonylee2797@users.noreply.github.com>
2020-12-01 17:55:57 -0500

    tab size 2
    tab size 2

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -6,7 +6,7 @@ function App(props) {
 	const [filter, setFilter] = useState('')
 
 	useEffect(() => { getData() }, [])
-
+ 
 	async function getData (){
 		const response = await fetch('https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city', {
 			headers: {
